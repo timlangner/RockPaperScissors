bdb5e38392f4b4fcef6cb222f781b713a8ec150e
Tim <tim.langner0@gmail.com>
2020-11-16 13:55:55 +0100

    :tada: Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+print('What do you want to choose?')
+userInput = input('Press 1, 2 or 3');
+
+if userInput == '1':
+    print('1')
+elif userInput == '2':
+    print('2')
+elif userInput == '3':
+    print('3')
+else:
+    print('Invalid input')
+
