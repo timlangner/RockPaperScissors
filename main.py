77128a0727f4f0dcd686f1a4e34393586133d4fa
Tim <tim.langner0@gmail.com>
2020-11-16 14:10:24 +0100

    :sparkles: Add game logic

--- main.py
+++ main.py
@@ -1,12 +1,37 @@
-print('What do you want to choose?')
-userInput = input('Press 1, 2 or 3');
+from random import randint
 
-if userInput == '1':
-    print('1')
-elif userInput == '2':
-    print('2')
-elif userInput == '3':
-    print('3')
-else:
-    print('Invalid input')
+#create a list of play options
+options = ['Rock', 'Paper', 'Scissors']
 
+#assign a random play to the computer
+computer = options[randint(0, 2)]
+
+#set player to false
+player = False
+
+while player == False:
+    #set player to True
+    player = input('Rock, Paper, Scissors?')
+    if player == computer:
+        print('Tie!')
+    elif player == 'Rock':
+        if computer == 'Paper':
+            print('You lose!', computer, 'covers', player)
+        else:
+            print('You win!', player, 'smashes', computer)
+    elif player == 'Paper':
+        if computer == 'Scissors':
+            print('You lose!', computer, 'cut', player)
+        else:
+            print('You win!', player, 'covers', computer)
+    elif player == 'Scissors':
+        if computer == 'Rock':
+            print('You lose...', computer, 'smashes', player)
+        else:
+            print('You win!', player, 'cut', computer)
+    else:
+        print("That's not a valid play. Check your spelling!")
+
+        #player was set to True, but we want it to be False so the loop continues
+        player = False
+        computer = options[randint(0, 2)]
